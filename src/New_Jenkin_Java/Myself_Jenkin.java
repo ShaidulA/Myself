5ed1dbe659da13fbf8daecd09926047f7b4cf3e5
shidul <shaidulazam@gmail.com>
2024-02-17 11:21:47 -0500

    Shaidul

---
+++ src/New_Jenkin_Java/Myself_Jenkin.java
@@ -0,0 +1,9 @@
+package New_Jenkin_Java;
+
+public class Myself_Jenkin {
+	public static void main(String[] args) {
+		System.out.println("My name is Shaidul Azam, I am a sales consultant. I live in Jamaica, New York.");
+
+	}
+
+}
